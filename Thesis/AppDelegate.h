41d5968ab9b4d8a68bd0d336ceedce2fc9b40487
Mark Vasiv <mark.vasiv@gmail.com>
2017-03-18 23:49:14 +0200

    Initial Commit

---
+++ Thesis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thesis
+//
+//  Created by Mark Vasiv on 18/03/2017.
+//  Copyright © 2017 Mark Vasiv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
